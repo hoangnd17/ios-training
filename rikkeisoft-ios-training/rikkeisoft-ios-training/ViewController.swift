a56c18d834f88397fdb31f558fce02309ad323a5
hoangnd2 <hoangnd2@rikkeisoft.com>
2021-08-05 17:13:58 +0800

    Initial project

---
+++ rikkeisoft-ios-training/rikkeisoft-ios-training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rikkeisoft-ios-training
+//
+//  Created by HoangND2 on 05/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
